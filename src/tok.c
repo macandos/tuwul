60afcc2b855cd025bd743a86b6e8aa7e8ed08d43
macandos <87715564+macandos@users.noreply.github.com>
2022-02-09 20:21:06 +0000

    added lexer

---
+++ src/tok.c
@@ -0,0 +1,117 @@
+#include "tok.h"
+
+int n = 0;
+int linenum = 1;
+
+Token* tokize(char character[], long filesize) {
+    static Token tokens[LENGTH];
+    memset(tokens, 0, LENGTH*sizeof(Token));
+    n = 0;
+
+    int getLinenum;
+    Token token;
+    for (int e = 0; e < filesize - linenum + 1; e++) {
+        if (token.tok == COMMENT) {
+            if (getLinenum < linenum) {
+                token = createTok(' ', WHITESPACE);
+                continue;
+            }
+        }
+
+        switch (character[e]) {
+            case '|':
+                token = appendTok(token, tokens);
+
+                getLinenum = linenum;
+                token = createTok('|', COMMENT);
+                continue;
+            case '\n':
+            case '\r':
+                token = appendTok(token, tokens);
+                ++linenum;
+                break;
+            case ' ':
+            case '\t':
+                if (token.tok == COMMENT) {
+                    strncat(token.text, &character[e], 1);
+                }
+                else {
+                    token = appendTok(token, tokens);
+                }
+                break;
+            default:
+                if (token.tok != IDENTIFIER && token.tok != COMMENT) {
+                    token = appendTok(token, tokens);
+                    token = createTok(character[e], IDENTIFIER);
+                }
+                else {
+                    strncat(token.text, &character[e], 1);
+                }
+                break;
+
+        }
+    }
+
+    token = appendTok(token, tokens);
+    
+    token = createTok(' ', ENDOF);
+    token = appendTok(token, tokens);
+    return tokens;
+}
+
+Token createTok(char name, Toks tok) {
+    Token token;
+    strcpy(token.text, "");
+    strncat(token.text, &name, 1);
+    token.tok = tok;
+
+    return token;
+}
+
+Token appendTok(Token token, Token tokens[]) {
+    if (token.tok != WHITESPACE && token.tok != COMMENT) {
+        token.linenum = linenum; 
+        tokens[n] = token;
+        n++;
+    }
+
+    Token returnTok;
+    returnTok.linenum = token.linenum;
+    returnTok.tok = WHITESPACE;
+
+    return returnTok;
+}
+
+Token *sortToks(Token* tokens) {
+    for (int i = 0; i < LENGTH; i++) {
+        if (tokens[i].tok == 0 || tokens[i].tok == ENDOF) break;
+
+        if (strcmp(tokens[i].text, "uwu") == 0) {
+            tokens[i].type = UWU;
+        }
+        else if (strcmp(tokens[i].text, "owo") == 0) {
+            tokens[i].type = OWO;
+        }
+        else if (strcmp(tokens[i].text, "ywy") == 0) {
+            tokens[i].type = YWY;
+        }
+        else if (strcmp(tokens[i].text, "twt") == 0) {
+            tokens[i].type = TWT;
+        }
+        else if (strcmp(tokens[i].text, "ewe") == 0) {
+            tokens[i].type = EWE;
+        }
+        else if (strcmp(tokens[i].text, "awa") == 0) {
+            tokens[i].type = AWA;
+        }
+        else if (strcmp(tokens[i].text, "iwi") == 0) {
+            tokens[i].type = IWI;
+        }
+    }
+
+    return tokens;
+}
+
+void debug(Token token) {
+    printf("token(%d, %s, %s, %d);\n", token.type, uwu[token.tok], token.text, token.linenum);
+}
---
+++ src/tok.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#ifndef TOK_H_
+#define TOK_H_
+
+#define LENGTH 10 * 1000
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "types.h"
+
+typedef enum {
+    WHITESPACE,
+
+    IDENTIFIER,
+    COMMENT,
+
+    // End of file
+    ENDOF
+} Toks;
+
+// for debug purposes
+static const char* uwu[] = {
+    "WHITESPACE",
+
+    "IDENTIFIER",
+    "COMMENT",
+
+    // End of file
+    "ENDOF"
+};
+
+typedef struct {
+    int linenum;
+    char text[LENGTH];
+    Toks tok;
+
+    Type type;
+} Token;
+
+Token* tokize(char character[], long filesize);
+Token createTok(char name, Toks tok);
+Token appendTok(Token token, Token tokens[]);
+Token *sortToks(Token* tokens);
+
+// debug
+void debug(Token token);
+
+#endif
